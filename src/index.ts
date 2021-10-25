fabiordrig/feedz-crawler

feat(feedz): adding route to apply daily humor

src/index.ts
@@ -1,11 +1,11 @@
 import express from "express";
 import { json } from "body-parser";
-import { dailyHumorRoute } from "./routes/daily-humor";
+import { getBets } from "./routes/get-bets";
 
 const app = express();
 app.use(json());
 
-app.use("/api", dailyHumorRoute);
+app.use("/api", getBets);
 
 app.listen(3000, () => {
   console.log("Listening on 3000");

fabiordrig/compliance

src/index.ts
@@ -1,11 +1,11 @@
 import express from "express";
 import { json } from "body-parser";
-import { dailyHumorRoute } from "./routes/daily-humor";
+import { getDataRouter } from "./routes/get-data";
 
 const app = express();
 app.use(json());
 
-app.use("/api", dailyHumorRoute);
+app.use("/api", getDataRouter);
 
 app.listen(3000, () => {
   console.log("Listening on 3000");